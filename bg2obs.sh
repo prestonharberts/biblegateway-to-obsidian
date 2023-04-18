e20fe29363d821bbeec9d90dfcadf91a68c38dca
Preston Harberts <41871405+prestonharberts@users.noreply.github.com>
2023-04-18 14:38:07 -0500

    refactor: remove parent folder

--- bg2obs.sh
+++ bg2obs.sh
@@ -27,8 +27,7 @@ biblenamefile="bible"
 # Name of the parent folder that the bible folder will be inside
 parentfolder="christianity"
 # Make folders
-mkdir -p "${parentfolder}"
-mkdir -p "${parentfolder}/${biblenamefile}"
+mkdir -p "${biblenamefile}"
 # Full names of the books of the Bible
 bookarraytext=("Book of Genesis" "Book of Exodus" "Book of Leviticus" "Book of Numbers" "Book of Deuteronomy" "Book of Joshua" "Book of Judges" "Book of Ruth" "First Book of Samuel" "Second Book of Samuel" "First Book of Kings" "Second Book of Kings" "First Book of Chronicles" "Second Book of Chronicles" "Book of Ezra" "Book of Nehemiah" "Book of Esther" "Book of Job" "Book of Psalms" "Book of Proverbs" "Book of Ecclesiastes" "Song of Solomon" "Book of Isaiah" "Book of Jeremiah" "Book of Lamentations" "Book of Ezekiel" "Book of Daniel" "Book of Hosea" "Book of Joel" "Book of Amos" "Book of Obadiah" "Book of Jonah" "Book of Micah" "Book of Nahum" "Book of Habakkuk" "Book of Zephaniah" "Book of Haggai" "Book of Zechariah" "Book of Malachi" "Gospel of Matthew" "Gospel of Mark" "Gospel of Luke" "Gospel of John" "Acts of the Apostles" "Epistle to the Romans" "First Epistle to the Corinthians" "Second Epistle to the Corinthians" "Epistle to the Galatians" "Epistle to the Ephesians" "Epistle to the Philippians" "Epistle to the Colossians" "First Epistle to the Thessalonians" "Second Epistle to the Thessalonians" "First Epistle to Timothy" "Second Epistle to Timothy" "Epistle to Titus" "Epistle to Philemon" "Epistle to the Hebrews" "Epistle of James" "First Epistle of Peter" "Second Epistle of Peter" "First Epistle of John" "Second Epistle of John" "Third Epistle of John" "Epistle of Jude" "Book of Revelation")
 bookarrayfile=(book-of-genesis book-of-exodus book-of-leviticus book-of-numbers book-of-deuteronomy book-of-joshua book-of-judges book-of-ruth first-book-of-samuel second-book-of-samuel first-book-of-kings second-book-of-kings first-book-of-chronicles second-book-of-chronicles book-of-ezra book-of-nehemiah book-of-esther book-of-job book-of-psalms book-of-proverbs book-of-ecclesiastes song-of-solomon book-of-isaiah book-of-jeremiah book-of-lamentations book-of-ezekiel book-of-daniel book-of-hosea book-of-joel book-of-amos book-of-obadiah book-of-jonah book-of-micah book-of-nahum book-of-habakkuk book-of-zephaniah book-of-haggai book-of-zechariah book-of-malachi gospel-of-matthew gospel-of-mark gospel-of-luke gospel-of-john acts-of-the-apostles epistle-to-the-romans first-epistle-to-the-corinthians second-epistle-to-the-corinthians epistle-to-the-galatians epistle-to-the-ephesians epistle-to-the-philippians epistle-to-the-colossians first-epistle-to-the-thessalonians second-epistle-to-the-thessalonians first-epistle-to-timothy second-epistle-to-timothy epistle-to-titus epistle-to-philemon epistle-to-the-hebrews epistle-of-james first-epistle-of-peter second-epistle-of-peter first-epistle-of-john second-epistle-of-john third-epistle-of-john epistle-of-jude book-of-revelation)
@@ -44,24 +43,24 @@ genrearrayfile=(pentateuch pentateuch pentateuch pentateuch pentateuch historica
 # -------------------------------------------
 
 # Initialize the Bible file for all of the Old/New Testament
-echo -e "# ${biblenametext}\n\n## Contents\n\n[[${testarrayfile[0]}|${testarraytext[0]}]]\n[[${testarrayfile[39]}|${testarraytext[39]}]]" >>"${parentfolder}/${biblenamefile}/${biblenamefile}.md"
+echo -e "# ${biblenametext}\n\n## Contents\n\n[[${testarrayfile[0]}|${testarraytext[0]}]]\n[[${testarrayfile[39]}|${testarraytext[39]}]]" >>"${biblenamefile}/${biblenamefile}.md"
 
 # Initialize the Old/New Testament file for all of the genres
-echo -e "# ${testarraytext[0]}\n\n## Contents\n\n[[${genrearrayfile[0]}|${genrearraytext[0]}]]\n[[${genrearrayfile[5]}|${genrearraytext[5]}]]\n[[${genrearrayfile[17]}|${genrearraytext[17]}]]\n[[${genrearrayfile[19]}|${genrearraytext[19]}]]\n[[${genrearrayfile[22]}|${genrearraytext[22]}]]\n[[${genrearrayfile[27]}|${genrearraytext[27]}]]" >>"${parentfolder}/${biblenamefile}/${testarrayfile[0]}.md"
-echo -e "# ${testarraytext[39]}\n\n## Contents\n\n[[${genrearrayfile[39]}|${genrearraytext[39]}]]\n[[${genrearrayfile[43]}|${genrearraytext[43]}]]\n[[${genrearrayfile[44]}|${genrearraytext[44]}]]\n[[${genrearrayfile[57]}|${genrearraytext[57]}]]\n[[${genrearrayfile[65]}|${genrearraytext[65]}]]" >>"${parentfolder}/${biblenamefile}/${testarrayfile[39]}.md"
+echo -e "# ${testarraytext[0]}\n\n## Contents\n\n[[${genrearrayfile[0]}|${genrearraytext[0]}]]\n[[${genrearrayfile[5]}|${genrearraytext[5]}]]\n[[${genrearrayfile[17]}|${genrearraytext[17]}]]\n[[${genrearrayfile[19]}|${genrearraytext[19]}]]\n[[${genrearrayfile[22]}|${genrearraytext[22]}]]\n[[${genrearrayfile[27]}|${genrearraytext[27]}]]" >>"${biblenamefile}/${testarrayfile[0]}.md"
+echo -e "# ${testarraytext[39]}\n\n## Contents\n\n[[${genrearrayfile[39]}|${genrearraytext[39]}]]\n[[${genrearrayfile[43]}|${genrearraytext[43]}]]\n[[${genrearrayfile[44]}|${genrearraytext[44]}]]\n[[${genrearrayfile[57]}|${genrearraytext[57]}]]\n[[${genrearrayfile[65]}|${genrearraytext[65]}]]" >>"${biblenamefile}/${testarrayfile[39]}.md"
 
 # Initialize the genre file for all of the Bible
-echo -e "# ${genrearraytext[0]}\n\n## Contents\n" >>"${parentfolder}/${biblenamefile}/${genrearrayfile[0]}.md"
-echo -e "# ${genrearraytext[5]}\n\n## Contents\n" >>"${parentfolder}/${biblenamefile}/${genrearrayfile[5]}.md"
-echo -e "# ${genrearraytext[17]}\n\n## Contents\n" >>"${parentfolder}/${biblenamefile}/${genrearrayfile[17]}.md"
-echo -e "# ${genrearraytext[19]}\n\n## Contents\n" >>"${parentfolder}/${biblenamefile}/${genrearrayfile[19]}.md"
-echo -e "# ${genrearraytext[22]}\n\n## Contents\n" >>"${parentfolder}/${biblenamefile}/${genrearrayfile[22]}.md"
-echo -e "# ${genrearraytext[27]}\n\n## Contents\n" >>"${parentfolder}/${biblenamefile}/${genrearrayfile[27]}.md"
-echo -e "# ${genrearraytext[39]}\n\n## Contents\n" >>"${parentfolder}/${biblenamefile}/${genrearrayfile[39]}.md"
-echo -e "# ${genrearraytext[43]}\n\n## Contents\n" >>"${parentfolder}/${biblenamefile}/${genrearrayfile[43]}.md"
-echo -e "# ${genrearraytext[44]}\n\n## Contents\n" >>"${parentfolder}/${biblenamefile}/${genrearrayfile[44]}.md"
-echo -e "# ${genrearraytext[57]}\n\n## Contents\n" >>"${parentfolder}/${biblenamefile}/${genrearrayfile[57]}.md"
-echo -e "# ${genrearraytext[65]}\n\n## Contents\n" >>"${parentfolder}/${biblenamefile}/${genrearrayfile[65]}.md"
+echo -e "# ${genrearraytext[0]}\n\n## Contents\n" >>"${biblenamefile}/${genrearrayfile[0]}.md"
+echo -e "# ${genrearraytext[5]}\n\n## Contents\n" >>"${biblenamefile}/${genrearrayfile[5]}.md"
+echo -e "# ${genrearraytext[17]}\n\n## Contents\n" >>"${biblenamefile}/${genrearrayfile[17]}.md"
+echo -e "# ${genrearraytext[19]}\n\n## Contents\n" >>"${biblenamefile}/${genrearrayfile[19]}.md"
+echo -e "# ${genrearraytext[22]}\n\n## Contents\n" >>"${biblenamefile}/${genrearrayfile[22]}.md"
+echo -e "# ${genrearraytext[27]}\n\n## Contents\n" >>"${biblenamefile}/${genrearrayfile[27]}.md"
+echo -e "# ${genrearraytext[39]}\n\n## Contents\n" >>"${biblenamefile}/${genrearrayfile[39]}.md"
+echo -e "# ${genrearraytext[43]}\n\n## Contents\n" >>"${biblenamefile}/${genrearrayfile[43]}.md"
+echo -e "# ${genrearraytext[44]}\n\n## Contents\n" >>"${biblenamefile}/${genrearrayfile[44]}.md"
+echo -e "# ${genrearraytext[57]}\n\n## Contents\n" >>"${biblenamefile}/${genrearrayfile[57]}.md"
+echo -e "# ${genrearraytext[65]}\n\n## Contents\n" >>"${biblenamefile}/${genrearrayfile[65]}.md"
 
 # Book chapter list
 lengtharray=(50 40 27 36 34 24 21 4 31 24 22 25 29 36 10 13 10 42 150 31 12 8 66 52 5 48 12 14 3 9 1 4 7 3 3 3 2 14 4 28 16 24 21 28 16 16 13 6 6 4 4 5 3 6 4 3 1 13 5 5 3 5 1 1 1 22)
@@ -83,10 +82,10 @@ for ((bookcounter = 0; bookcounter <= bookcountermax; bookcounter++)); do
   echo -n "${booktext} "
 
   # Create an overview file for each book of the Bible:
-  echo -e "# ${booktext}\n\n## Contents\n" >>"${parentfolder}/${biblenamefile}/$bookfile.md"
+  echo -e "# ${booktext}\n\n## Contents\n" >>"${biblenamefile}/$bookfile.md"
 
   # Create an overview file for each book of the Bible:
-  echo -e "[[${bookfile}|${booktext}]]" >>"${parentfolder}/${biblenamefile}/${genrefile}.md"
+  echo -e "[[${bookfile}|${booktext}]]" >>"${biblenamefile}/${genrefile}.md"
 
   for ((chapter = 1; chapter <= maxchapter; chapter++)); do
 
@@ -103,9 +102,9 @@ for ((bookcounter = 0; bookcounter <= bookcountermax; bookcounter++)); do
     nextfile=${exportprefix}$nextchapter
 
     if [[ $chapter -gt 1 ]]; then
-      echo -e "[[${abbfile}-$chapter|]]" >>"${parentfolder}/${biblenamefile}/$bookfile.md"
+      echo -e "[[${abbfile}-$chapter|]]" >>"${biblenamefile}/$bookfile.md"
     else
-      echo -e "[[${abbfile}-$chapter|Start reading]]" >>"${parentfolder}/${biblenamefile}/$bookfile.md"
+      echo -e "[[${abbfile}-$chapter|Start reading]]" >>"${biblenamefile}/$bookfile.md"
     fi
 
     audiobible="![[${abbfile}-$chapter.mp3]]"
@@ -137,8 +136,8 @@ for ((bookcounter = 0; bookcounter <= bookcountermax; bookcounter++)); do
     export="$title\n\n$audiobible\n$text\n\n$navigation"
 
     # Export
-    echo -e "$export" >>"./${parentfolder}/${biblenamefile}/${filename}.md"
-    echo -e "$export" >>"./${parentfolder}/${biblenamefile}/${filename}-notes.md"
+    echo -e "$export" >>"./${biblenamefile}/${filename}.md"
+    echo -e "$export" >>"./${biblenamefile}/${filename}-notes.md"
 
   done # End of the book exporting loop
 
