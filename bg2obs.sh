4af700358ac41419f86499c5d5aac10ae18302b3
Preston Harberts <41871405+prestonharberts@users.noreply.github.com>
2023-04-22 12:35:57 -0500

    refactor: split audio outline creation

--- bg2obs.sh
+++ bg2obs.sh
@@ -1,54 +1,8 @@
 #!/bin/bash
 
 source config
-source src/variables
-
-mkdir -p "${bible_dir}" "${audio_dir}" "${outline_dir}" "${notes_dir}" "${reading_dir}"
-
-# Name of the audio Bible file
-audio_bible_name="Audio Bible"
-standard_audio_bible_name=$(echo "${audio_bible_name}" | sed 's/ /-/g; s/.*/\L&/g')
-# Initialize audio Bible file
-echo -e "## ${audio_bible_name}\n" >>"${outline_dir}/${standard_audio_bible_name}.md"
-
-for ((book = 0; book < book_max; book++)); do
-  short_title="${short_title_arr[$book]}"
-  standard_abbreviation="${standard_abbreviation_arr[$book]}"
-  chapter_max=${length_arr[$book]}
-
-  if [[ $book != 0 ]]; then
-    ((previous_book_num = book - 1))
-  fi
-  ((next_book_num = book + 1))
-
-  previous_book="${short_title[previous_book_num]}"
-  next_book="${short_title[next_book_num]}"
-
-  for ((chapter = 1; chapter <= chapter_max; chapter++)); do
-    title="${short_title} ${chapter}"
-    audio_bible="![[${standard_abbreviation}${chapter}.mp3]]"
-    echo -e "${title}\n${audio_bible}" >>"${outline_dir}/${standard_abbreviation}-audio.md"
-  done
-
-  # Formatting Navigation and omitting links that aren't necessary
-  if [[ $book -eq 0 ]]; then
-    # For Genesis
-    navigation="\n[[${standard_audio_bible_name}|All books]]\n[[${next_book}-audio|Next book]]"
-  elif [[ $book -eq 65 ]]; then
-    # For Revelation
-    navigation="\n[[${standard_audio_bible_name}|All books]]\n[[${previous_book}-audio|Previous book]]"
-    # For every other book
-  else
-    # Navigation for everything else
-    navigation="\n[[${standard_audio_bible_name}|All books]]\n[[${previous_book}-audio|Previous book]]\n[[${next_book}-audio|Next book]]"
-  fi
-
-  if [[ "${navigation}" != "" ]]; then
-    echo -e "${navigation}" >>"${outline_dir}/${standard_abbreviation}-audio.md"
-  fi
-
-  echo -e "[[${standard_abbreviation}-audio|${short_title}]]" >>"${outline_dir}/${standard_audio_bible_name}.md"
-done
+source src/setup
+source src/outline
 
 echo "Starting download of the ${translation} Bible."
 
@@ -56,12 +10,12 @@ echo "Starting download of the ${translation} Bible."
 for ((book = 0; book < book_max; book++)); do
   echo "" # Make a new line which the '-n' flag to the echo command prevents.
 
-  long_title="${long_title_arr[$book]}"
-  short_title="${short_title_arr[$book]}"
-  standard_long_title="${standard_long_title_arr[$book]}"
-  standard_abbreviation="${standard_abbreviation_arr[$book]}"
-  standard_genre="${standard_genre_arr[$book]}"
-  chapter_max=${length_arr[$book]}
+  long_title="${long_title_arr[book]}"
+  short_title="${short_title_arr[book]}"
+  standard_long_title="${standard_long_title_arr[book]}"
+  standard_abbreviation="${standard_abbreviation_arr[book]}"
+  standard_genre="${standard_genre_arr[book]}"
+  chapter_max=${length_arr[book]}
 
   echo -n "${short_title} "
 
@@ -115,3 +69,5 @@ for ((book = 0; book < book_max; book++)); do
     echo -e "# ${short_title} ${chapter}\n\n${audio_bible}\n${text}\n\n${navigation}" >>"${notes_dir}/${filename}-notes.md"
   done # End of the book exporting loop
 done
+
+source src/cleanup
