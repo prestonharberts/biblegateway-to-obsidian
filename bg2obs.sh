86873db868f26962b8c7b3b175db751728dabd5c
Preston Harberts <41871405+prestonharberts@users.noreply.github.com>
2023-04-22 11:58:33 -0500

    feat: allow user config

--- bg2obs.sh
+++ bg2obs.sh
@@ -3,54 +3,36 @@
 source config
 source src/variables
 
-# Create kebab-cased variables
-standard_translation=$(echo "${translation}" | sed 's/ /-/g; s/.*/\L&/g')
-standard_bible_name=$(echo "${bible_name}" | sed 's/ /-/g; s/.*/\L&/g')
-for ((book = 0; book < book_max; book++)); do
-  standard_long_title_array[book]=$(echo "${long_title_array[$book]}" | sed 's/ /-/g; s/.*/\L&/g')
-  standard_short_title_array[book]=$(echo "${short_title_array[$book]}" | sed 's/ /-/g; s/.*/\L&/g')
-  standard_abbreviation_array[book]=$(echo "${abbreviation_array[$book]}" | sed 's/ //g; s/.*/\L&/g; s/\.//g')
-  standard_testament_array[book]=$(echo "${testament_array[$book]}" | sed 's/ /-/g; s/.*/\L&/g')
-  standard_genre_array[book]=$(echo "${genre_array[$book]}" | sed 's/ /-/g; s/.*/\L&/g')
-done
-
-bible_folder="./${standard_bible_name}-${standard_translation}"
-audio_folder="${bible_folder}/audio"
-contents_folder="${bible_folder}/contents"
-notes_folder="${bible_folder}/notes"
-reading_folder="${bible_folder}/reading"
-
-mkdir -p "${bible_folder}"
-mkdir -p "${audio_folder}" "${contents_folder}" "${notes_folder}" "${reading_folder}"
+mkdir -p "${bible_folder}" "${audio_dir}" "${contents_dir}" "${notes_dir}" "${reading_dir}"
 
 # Initialize the Bible file for all of the Old/New Testament
-echo -e "# ${bible_name}\n\n## Contents\n\n[[${standard_testament_array[0]}|${testament_array[0]}]]\n[[${standard_testament_array[39]}|${testament_array[39]}]]" >>"${contents_folder}/${standard_bible_name}.md"
+echo -e "# ${bible_name}\n\n## Contents\n\n[[${standard_testament_arr[0]}|${testament_arr[0]}]]\n[[${standard_testament_arr[39]}|${testament_arr[39]}]]" >>"${contents_dir}/${standard_bible_name}.md"
 # Initialize the Old/New Testament file for all of the genres
-echo -e "# ${testament_array[0]}\n\n## Contents\n\n[[${standard_genre_array[0]}|${genre_array[0]}]]\n[[${standard_genre_array[5]}|${genre_array[5]}]]\n[[${standard_genre_array[17]}|${genre_array[17]}]]\n[[${standard_genre_array[19]}|${genre_array[19]}]]\n[[${standard_genre_array[22]}|${genre_array[22]}]]\n[[${standard_genre_array[27]}|${genre_array[27]}]]" >>"${contents_folder}/${standard_testament_array[0]}.md"
-echo -e "# ${testament_array[39]}\n\n## Contents\n\n[[${standard_genre_array[39]}|${genre_array[39]}]]\n[[${standard_genre_array[43]}|${genre_array[43]}]]\n[[${standard_genre_array[44]}|${genre_array[44]}]]\n[[${standard_genre_array[57]}|${genre_array[57]}]]\n[[${standard_genre_array[65]}|${genre_array[65]}]]" >>"${contents_folder}/${standard_testament_array[39]}.md"
+echo -e "# ${testament_arr[0]}\n\n## Contents\n\n[[${standard_genre_arr[0]}|${genre_arr[0]}]]\n[[${standard_genre_arr[5]}|${genre_arr[5]}]]\n[[${standard_genre_arr[17]}|${genre_arr[17]}]]\n[[${standard_genre_arr[19]}|${genre_arr[19]}]]\n[[${standard_genre_arr[22]}|${genre_arr[22]}]]\n[[${standard_genre_arr[27]}|${genre_arr[27]}]]" >>"${contents_dir}/${standard_testament_arr[0]}.md"
+echo -e "# ${testament_arr[39]}\n\n## Contents\n\n[[${standard_genre_arr[39]}|${genre_arr[39]}]]\n[[${standard_genre_arr[43]}|${genre_arr[43]}]]\n[[${standard_genre_arr[44]}|${genre_arr[44]}]]\n[[${standard_genre_arr[57]}|${genre_arr[57]}]]\n[[${standard_genre_arr[65]}|${genre_arr[65]}]]" >>"${contents_dir}/${standard_testament_arr[39]}.md"
 # Initialize the genre file for all of the Bible
-echo -e "# ${genre_array[0]}\n\n## Contents\n" >>"${contents_folder}/${standard_genre_array[0]}.md"
-echo -e "# ${genre_array[5]}\n\n## Contents\n" >>"${contents_folder}/${standard_genre_array[5]}.md"
-echo -e "# ${genre_array[17]}\n\n## Contents\n" >>"${contents_folder}/${standard_genre_array[17]}.md"
-echo -e "# ${genre_array[19]}\n\n## Contents\n" >>"${contents_folder}/${standard_genre_array[19]}.md"
-echo -e "# ${genre_array[22]}\n\n## Contents\n" >>"${contents_folder}/${standard_genre_array[22]}.md"
-echo -e "# ${genre_array[27]}\n\n## Contents\n" >>"${contents_folder}/${standard_genre_array[27]}.md"
-echo -e "# ${genre_array[39]}\n\n## Contents\n" >>"${contents_folder}/${standard_genre_array[39]}.md"
-echo -e "# ${genre_array[43]}\n\n## Contents\n" >>"${contents_folder}/${standard_genre_array[43]}.md"
-echo -e "# ${genre_array[44]}\n\n## Contents\n" >>"${contents_folder}/${standard_genre_array[44]}.md"
-echo -e "# ${genre_array[57]}\n\n## Contents\n" >>"${contents_folder}/${standard_genre_array[57]}.md"
-echo -e "# ${genre_array[65]}\n\n## Contents\n" >>"${contents_folder}/${standard_genre_array[65]}.md"
+echo -e "# ${genre_arr[0]}\n\n## Contents\n" >>"${contents_dir}/${standard_genre_arr[0]}.md"
+echo -e "# ${genre_arr[5]}\n\n## Contents\n" >>"${contents_dir}/${standard_genre_arr[5]}.md"
+echo -e "# ${genre_arr[17]}\n\n## Contents\n" >>"${contents_dir}/${standard_genre_arr[17]}.md"
+echo -e "# ${genre_arr[19]}\n\n## Contents\n" >>"${contents_dir}/${standard_genre_arr[19]}.md"
+echo -e "# ${genre_arr[22]}\n\n## Contents\n" >>"${contents_dir}/${standard_genre_arr[22]}.md"
+echo -e "# ${genre_arr[27]}\n\n## Contents\n" >>"${contents_dir}/${standard_genre_arr[27]}.md"
+echo -e "# ${genre_arr[39]}\n\n## Contents\n" >>"${contents_dir}/${standard_genre_arr[39]}.md"
+echo -e "# ${genre_arr[43]}\n\n## Contents\n" >>"${contents_dir}/${standard_genre_arr[43]}.md"
+echo -e "# ${genre_arr[44]}\n\n## Contents\n" >>"${contents_dir}/${standard_genre_arr[44]}.md"
+echo -e "# ${genre_arr[57]}\n\n## Contents\n" >>"${contents_dir}/${standard_genre_arr[57]}.md"
+echo -e "# ${genre_arr[65]}\n\n## Contents\n" >>"${contents_dir}/${standard_genre_arr[65]}.md"
 
 # Name of the audio Bible file
 audio_bible_name="Audio Bible"
 standard_audio_bible_name=$(echo "${audio_bible_name}" | sed 's/ /-/g; s/.*/\L&/g')
 # Initialize audio Bible file
-echo -e "## ${audio_bible_name}\n" >>"${contents_folder}/${standard_audio_bible_name}.md"
+echo -e "## ${audio_bible_name}\n" >>"${contents_dir}/${standard_audio_bible_name}.md"
 
 for ((book = 0; book < book_max; book++)); do
-  short_title="${short_title_array[$book]}"
-  standard_abbreviation="${standard_abbreviation_array[$book]}"
-  chapter_max=${length_array[$book]}
+  short_title="${short_title_arr[$book]}"
+  standard_abbreviation="${standard_abbreviation_arr[$book]}"
+  chapter_max=${length_arr[$book]}
 
   if [[ $book != 0 ]]; then
     ((previous_book_num = book - 1))
@@ -63,7 +45,7 @@ for ((book = 0; book < book_max; book++)); do
   for ((chapter = 1; chapter <= chapter_max; chapter++)); do
     title="${short_title} ${chapter}"
     audio_bible="![[${standard_abbreviation}${chapter}.mp3]]"
-    echo -e "${title}\n${audio_bible}" >>"${contents_folder}/${standard_abbreviation}-audio.md"
+    echo -e "${title}\n${audio_bible}" >>"${contents_dir}/${standard_abbreviation}-audio.md"
   done
 
   # Formatting Navigation and omitting links that aren't necessary
@@ -80,10 +62,10 @@ for ((book = 0; book < book_max; book++)); do
   fi
 
   if [[ "${navigation}" != "" ]]; then
-    echo -e "${navigation}" >>"${contents_folder}/${standard_abbreviation}-audio.md"
+    echo -e "${navigation}" >>"${contents_dir}/${standard_abbreviation}-audio.md"
   fi
 
-  echo -e "[[${standard_abbreviation}-audio|${short_title}]]" >>"${contents_folder}/${standard_audio_bible_name}.md"
+  echo -e "[[${standard_abbreviation}-audio|${short_title}]]" >>"${contents_dir}/${standard_audio_bible_name}.md"
 done
 
 echo "Starting download of the ${translation} Bible."
@@ -92,19 +74,19 @@ echo "Starting download of the ${translation} Bible."
 for ((book = 0; book < book_max; book++)); do
   echo "" # Make a new line which the '-n' flag to the echo command prevents.
 
-  long_title="${long_title_array[$book]}"
-  short_title="${short_title_array[$book]}"
-  standard_long_title="${standard_long_title_array[$book]}"
-  standard_abbreviation="${standard_abbreviation_array[$book]}"
-  standard_genre="${standard_genre_array[$book]}"
-  chapter_max=${length_array[$book]}
+  long_title="${long_title_arr[$book]}"
+  short_title="${short_title_arr[$book]}"
+  standard_long_title="${standard_long_title_arr[$book]}"
+  standard_abbreviation="${standard_abbreviation_arr[$book]}"
+  standard_genre="${standard_genre_arr[$book]}"
+  chapter_max=${length_arr[$book]}
 
   echo -n "${short_title} "
 
   # Create an overview file for each book of the Bible:
-  echo -e "# ${long_title}\n\n## Contents\n" >>"${contents_folder}/${standard_long_title}.md"
+  echo -e "# ${long_title}\n\n## Contents\n" >>"${contents_dir}/${standard_long_title}.md"
   # Create an overview file for each book of the Bible:
-  echo -e "[[${standard_long_title}|${long_title}]]" >>"${contents_folder}/${standard_genre}.md"
+  echo -e "[[${standard_long_title}|${long_title}]]" >>"${contents_dir}/${standard_genre}.md"
 
   for ((chapter = 1; chapter <= chapter_max; chapter++)); do
     echo -n "."
@@ -119,9 +101,9 @@ for ((book = 0; book < book_max; book++)); do
     next_file="${export_prefix}${nextchapter}"
 
     if [[ $chapter -gt 1 ]]; then
-      echo -en "[[${export_prefix}${chapter}|]]" >>"${contents_folder}/${standard_long_title}.md"
+      echo -en "[[${export_prefix}${chapter}|]]" >>"${contents_dir}/${standard_long_title}.md"
     else
-      echo -en "[[${export_prefix}${chapter}|Start reading]]" >>"${contents_folder}/${standard_long_title}.md"
+      echo -en "[[${export_prefix}${chapter}|Start reading]]" >>"${contents_dir}/${standard_long_title}.md"
     fi
 
     audio_bible="![[${filename}.mp3]]"
@@ -147,7 +129,7 @@ for ((book = 0; book < book_max; book++)); do
     text="${text/^(.*?)v1/v1/}" # Deleting unwanted headers
 
     # Export
-    echo -e "# ${short_title} ${chapter}\n\n${audio_bible}\n${text}\n\n${navigation}" >>"${reading_folder}/${filename}.md"
-    echo -e "# ${short_title} ${chapter}\n\n${audio_bible}\n${text}\n\n${navigation}" >>"${notes_folder}/${filename}-notes.md"
+    echo -e "# ${short_title} ${chapter}\n\n${audio_bible}\n${text}\n\n${navigation}" >>"${reading_dir}/${filename}.md"
+    echo -e "# ${short_title} ${chapter}\n\n${audio_bible}\n${text}\n\n${navigation}" >>"${notes_dir}/${filename}-notes.md"
   done # End of the book exporting loop
 done
